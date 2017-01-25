d9d63067cbf6d3692a03052f53f1278d3755e248
Jakub Budzynski <james.codestudy@gmail.com>
2017-01-25 22:15:25 +0100

    Better Vending Machine System

--- Algo/Function.cpp
+++ Algo/Function.cpp
@@ -5,21 +5,12 @@ using namespace std;
 void loadVending(vector<pair<double, int>>& data)
 {
 	vector<double> nominals = { 200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01 };
-	int i = 0;
+	int i = 0, howManyNominals;
 	while (i<14)
 	{
-		char answ;
-		cout << "Czy jest " << nominals[i] << "  [t,n]: ";
-		cin >> answ;
-		if (answ != 't')
-		{
-			i++;
-			continue;
-		}
-		int number;
-		cout<< "Ile monet/banknotow: ";
-		cin >> number;
-		data.push_back(make_pair(nominals[i],number));
+		cout << "Ilosc " << nominals[i] << ": ";
+		cin >> howManyNominals;
+		data.push_back(make_pair(nominals[i],howManyNominals));
 		i++;
 	}
 	system("cls");

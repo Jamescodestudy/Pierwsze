6cbb0c6b0cb75c1a333c2e7d6b92c761eec23be4
Jakub Budzynski <james.codestudy@gmail.com>
2017-01-25 21:45:01 +0100

    First Version

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
-
+	// Nonima³y
+	vector<int> N = { 200,100,50,20,10,5,2,1 };
+	vector<int> result;
+	int i = 0, kwota = 148;
+	int howManyTimes;
+	while (kwota > 0)
+	{
+		if (kwota >= N[i])
+		{
+			howManyTimes = kwota / N[i];
+			for(int j=0; j<howManyTimes;j++) result.ADD(N[i]);
+			kwota -= (N[i] * howManyTimes);
+		}
+		i++;
+	}
 	getchar(); getchar();
 	return 0;
 }
